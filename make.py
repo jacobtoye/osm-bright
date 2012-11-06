b6cc273546e724f2b4b3974c8b136d1fb6b02736
jacobtoye <jacob.toye@gmail.com>
2012-11-06 13:15:17 +1300

    Added support for different themes. Uses new folder structure.

--- make.py
+++ make.py
@@ -21,6 +21,8 @@ from lib.utils import copy_tree
 
 config["path"] = expanduser(config["path"])
 
+theme_folder = "themes/" + config["theme"];
+
 def clean():
   if isdir("build"):
     rmtree("build")
@@ -29,14 +31,14 @@ def clean():
 
 def build():
   #copy the theme tree to a build dir
-  copy_tree(config["theme"], "build")
+  copy_tree(theme_folder, "build")
 
   #remove the mml templates
   for f in glob("build/*.mml"):
     unlink(f)
 
   #load the project template
-  templatefile = open(join(config["theme"], config["theme"] + ('.%s.mml' % config["importer"])))
+  templatefile = open(join(theme_folder, config["theme"] + ('.%s.mml' % config["importer"])))
   template = loads(templatefile.read())
 
   #fill in the project template
@@ -71,10 +73,10 @@ def pull():
   #copy the project from mapbox to the theme folder
   sanitized_name = re.sub("[^\w]", "", config["name"])
   output_dir = join(config["path"], sanitized_name)
-  copy_tree(output_dir, config["theme"], ("layers", ".thumb.png"))
+  copy_tree(output_dir, theme_folder, ("layers", ".thumb.png"))
 
   #load the project file
-  project = loads(open(join(config["theme"], "project.mml")).read())
+  project = loads(open(join(theme_folder, "project.mml")).read())
 
   #Make sure we reset postgis data in the project file back to its default values
   defaultconfig = defaultdict(defaultdict)
@@ -102,11 +104,11 @@ def pull():
         elif opt in layer["Datasource"]:
           del layer["Datasource"][opt]
 
-  project_template = open(join(config["theme"], config["theme"] + ".%s.mml") % config["importer"], 'w')
+  project_template = open(join(theme_folder, config["theme"] + ".%s.mml") % config["importer"], 'w')
   project_template.write(dumps(project, sort_keys=True, indent=2))
 
   #now delete project.mml
-  unlink(join(config["theme"], "project.mml"))
+  unlink(join(theme_folder, "project.mml"))
 
 if __name__ == "__main__":
   if sys.argv[-1] == "clean":
